bb51d1055b17451973c0ad835f2a5572bd54820c
bms_007 <168493571+Bernardinomarques007@users.noreply.github.com>
2025-08-26 15:48:26 -0300

    Add files via upload
    só funciona com o arquivo classe_ex_marinha1 
    data - 26\08\2025

---
+++ classe_ex_02.py
@@ -0,0 +1,28 @@
+from classe_ex_marinha1 import AnimaisMárinhos
+
+
+animal1 = AnimaisMárinhos('golfinho', 'cinza', 2.00, 'macho', 'não é')
+animal2 = AnimaisMárinhos('tubarão', 'azul', 3.50, 'fêmea', 'não é')
+animal3 = AnimaisMárinhos('baleia', 'preta', 15.00, 'fêmea', 'não é')
+animal4 = AnimaisMárinhos('polvo', 'vermelho', 1.20, 'macho', 'não é')
+animal5 = AnimaisMárinhos('arraia', 'cinza', 2.30, 'fêmea', 'não é')
+animal6 = AnimaisMárinhos('foca', 'branca', 1.80, 'fêmea', 'não é')
+animal7 = AnimaisMárinhos('leão-marinho', 'marrom', 2.10, 'macho', 'não é')
+animal8 = AnimaisMárinhos('cavalo-marinho', 'amarelo', 0.15, 'macho', 'não é')
+animal9 = AnimaisMárinhos('peixe-palhaço', 'laranja', 0.20, 'fêmea', 'não é')
+animal10 = AnimaisMárinhos('tartaruga-marinha', 'verde', 1.00, 'macho', 'não é')
+animal11 = AnimaisMárinhos('golfinho-rosa', 'rosa', 2.40, 'fêmea', 'não é')
+animal12 = AnimaisMárinhos('orca', 'preto e branco', 6.00, 'macho', 'não é')
+animal13 = AnimaisMárinhos('peixe-espada', 'prateado', 2.50, 'macho', 'não é')
+animal14 = AnimaisMárinhos('medusa', 'transparente', 0.50, 'fêmea', 'não é')
+animal15 = AnimaisMárinhos('caranguejo', 'vermelho', 0.30, 'macho', 'não é')
+animal16 = AnimaisMárinhos('lagosta', 'vermelho', 0.80, 'fêmea', 'não é')
+animal17 = AnimaisMárinhos('narval', 'cinza', 4.00, 'macho', 'não é')
+animal18 = AnimaisMárinhos('peixe-boi', 'cinza', 3.20, 'fêmea', 'não é')
+animal19 = AnimaisMárinhos('enguia', 'preta', 1.50, 'macho', 'não é')
+animal20 = AnimaisMárinhos('estrela-do-mar', 'laranja', 0.25, 'fêmea', 'não é')
+
+
+
+animal20.alimenticio()
+animal11.anfibio1()
